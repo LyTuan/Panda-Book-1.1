e5193fece581846aaa58f7d9f0b416a62d003738
TUẤN LÝ <lytuanduong96@gmail.com>
2016-10-19 16:05:54 +0700

    First commit

--- js/scriptShop.js
+++ js/scriptShop.js
@@ -3,3 +3,86 @@ $('ul.nav li.dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+jQuery(document).ready(function ($) {
+            
+            var jssor_options = {
+              $AutoPlay: true,
+              $AutoPlaySteps: 4,
+              $SlideDuration: 160,
+              $SlideWidth: 200,
+              $SlideSpacing: 3,
+              $Cols: 5,
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,
+                $Steps: 5
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$,
+                $SpacingX: 1,
+                $SpacingY: 1
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_options);
+            var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_options);
+            
+            var jssor_3_slider = new $JssorSlider$("jssor_3", jssor_options);
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 1200);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            function ScaleSlider2() {
+                var refSize = jssor_2_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 1200);
+                    jssor_2_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+              function ScaleSlider3() {
+                var refSize = jssor_3_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 1200);
+                    jssor_3_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            ScaleSlider2();
+            $(window).bind("load", ScaleSlider2);
+            $(window).bind("resize", ScaleSlider2);
+            $(window).bind("orientationchange", ScaleSlider2);
+            ScaleSlider3();
+            $(window).bind("load", ScaleSlider3);
+            $(window).bind("resize", ScaleSlider3);
+            $(window).bind("orientationchange", ScaleSlider3);
+            //responsive code end
+});
+// Get the modal
+    var modal = document.getElementById('idmodal');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            document.getElementById('idlogin').style.display='none';
+            document.getElementById('idregister').style.display='none';
+        }
+    }
